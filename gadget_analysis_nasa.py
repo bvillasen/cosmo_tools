80daf3800ed1d1cd90b6892aede0e55d401c3aa7
bruno <bvillasen@gmail.com>
2019-09-07 17:12:22 -0700

    updated tools

--- gadget_analysis_nasa.py
+++ gadget_analysis_nasa.py
@@ -12,7 +12,16 @@ from load_data_gadget import *
 simulation_directory = '/u/bvillase/dm/cosmo_2048/'
 data_directory = simulation_directory + 'set_0/data/'
 
+z_list = []
+snap_list = []
 
-nSnap = 0
-snapshot_info = get_snapshpt_info( nSnap, data_directory, single_file=False )
-print snapshot_info['current_z']
+for nSnap in range(58):
+  snapshot_info = get_snapshpt_info( nSnap, data_directory, single_file=False )
+  z = snapshot_info['current_z']
+  z_list.append( z )
+  snap_list.append( nSnap )
+  
+out_data = np.array([ snap_list, z_list ])  
+  
+out_file_name = 'cosmo_2048_outputs.txt'
+np.savetxt( out_file_name, out_data )
