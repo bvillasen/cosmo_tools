278434726b7726463d889b1dd5dfe331e627a9e7
bruno <bvillasen@gmail.com>
2019-09-07 17:26:00 -0700

    updated

--- gadget_analysis_nasa.py
+++ gadget_analysis_nasa.py
@@ -12,18 +12,26 @@ from load_data_gadget import *
 simulation_directory = '/u/bvillase/dm/cosmo_2048/'
 data_directory = simulation_directory + 'set_0/data/'
 
-z_list = []
-snap_list = []
+from os import listdir
 
-for nSnap in range(3):
-  nSnap = 0
-  snapshot_info = get_snapshpt_info( nSnap, data_directory, single_file=False )
-  z = snapshot_info['current_z']
-  z_list.append( z )
-  snap_list.append( nSnap )
-  
-out_data = np.array([ snap_list, z_list ]).T 
-  
-out_file_name = 'cosmo_2048_outputs.txt'
-header = 'n_snapshot   redshift'
-np.savetxt( out_file_name, out_data, fmt='%03d  %.6e', header=header )
+inDir = data_directory
+file_name_base = 'snapshot'
+
+dataFiles = [f for f in listdir(inDir) if (isfile(join(inDir, f)) and (f.find(snapshot) == 0 )  ]
+
+print( dataFiles )
+# 
+# z_list = []
+# snap_list = []
+# 
+# for nSnap in range(3):
+#   snapshot_info = get_snapshpt_info( nSnap, data_directory, single_file=False )
+#   z = snapshot_info['current_z']
+#   z_list.append( z )
+#   snap_list.append( nSnap )
+# 
+# out_data = np.array([ snap_list, z_list ]).T 
+# 
+# out_file_name = 'cosmo_2048_outputs.txt'
+# header = 'n_snapshot   redshift'
+# np.savetxt( out_file_name, out_data, fmt='%03d  %.6e', header=header )
