121b14d45fc2653a46b76cb5bacc0518d0eff5bb
Bruno <bvillasen@gmail.com>
2020-01-02 17:31:29 -0600

    updated

--- tools/compress_snapshots.py
+++ tools/compress_snapshots.py
@@ -32,7 +32,6 @@ if hydro:
 else:
   dataFiles = [f for f in listdir(inDir) if (isfile(join(inDir, f)) and ( f.find('_particles') > 0) ) ]
   
-dataFiles = [f for f in listdir(inDir) if (isfile(join(inDir, f)) and (f.find('.h5.') > 0 ) and ( f.find('_particles') < 0) ) ]
 dataFiles = np.sort( dataFiles )
 nFiles = len( dataFiles )
 
