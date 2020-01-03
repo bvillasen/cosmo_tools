ad8673c511ce51708cdd6565bbf0672947dfe185
Bruno <bvillasen@gmail.com>
2020-01-02 21:22:23 -0600

    fixed

--- tools/compress_snapshots.py
+++ tools/compress_snapshots.py
@@ -61,7 +61,7 @@ snapshots_to_compress = snapshots_all
 n_to_compress = len(snapshots_to_compress)
 print( "\nNumber of snapshots to compres: {0}".format(n_to_compress) )
 
-n_proc_runs = (n_to_compress-1)/nprocs + 1
+n_proc_runs = (n_to_compress-1) // nprocs + 1
 
 proc_runs = np.array([ rank + i*nprocs for i in range(n_proc_runs) ])
 proc_runs = proc_runs[ proc_runs < n_to_compress ]
