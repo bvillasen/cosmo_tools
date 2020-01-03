764fba4f2aac5eef8c1bac688202474ccd174cf5
Bruno <bvillasen@gmail.com>
2020-01-03 17:01:05 -0600

    updated power

--- analysis/power_spectrum/get_power_spectrum_dm_2048.py
+++ analysis/power_spectrum/get_power_spectrum_dm_2048.py
@@ -28,7 +28,8 @@ from tools import *
 # rank = comm.Get_rank()
 # nSnap = rank
 # 
-dataDir = '/data/groups/comp-astro/bruno/'
+dataDir = '/gpfs/alpine/proj-shared/ast149/'
+# dataDir = '/data/groups/comp-astro/bruno/'
 # dataDir = '/home/bruno/Desktop/data/'
 # dataDir = '/raid/bruno/data/'
 
