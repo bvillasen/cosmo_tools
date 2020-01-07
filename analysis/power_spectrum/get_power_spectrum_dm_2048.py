071e531904b8b833cc235ffce11b2143cbc33632
Bruno <bvillasen@gmail.com>
2020-01-07 11:11:56 -0600

    updated power spectrum

--- analysis/power_spectrum/get_power_spectrum_dm_2048.py
+++ analysis/power_spectrum/get_power_spectrum_dm_2048.py
@@ -1,7 +1,7 @@
 import sys, os, time
 import numpy as np
 import h5py as h5
-import pyfftw
+# import pyfftw
 import matplotlib
 # set some global options
 # matplotlib.font_manager.findSystemFonts(fontpaths=['/home/bruno/Downloads'], fontext='ttf')
@@ -13,14 +13,14 @@ cosmo_dir = os.path.dirname(os.path.dirname(os.getcwd())) + '/'
 dataDir = cosmo_dir + 'data/'
 subDirectories = [x[0] for x in os.walk(cosmo_dir)]
 sys.path.extend(subDirectories)
-from power_spectrum import get_power_spectrum
+from power_spectrum import get_power_spectrum, get_power_spectrum_fftw
 from load_data_cholla import load_snapshot_data, load_snapshot_data_particles
 from tools import *
 
 
-if len(sys.argv) == 0: index = 0
-else: index = int(sys.argv[1])
-print 'Index: ', index
+# if len(sys.argv) == 0: index = 0
+# else: index = int(sys.argv[1])
+# print 'Index: ', index
 
 # 
 # from mpi4py import MPI
@@ -43,6 +43,8 @@ nPoints = 2048
 chollaDir = dataDir + 'cosmo_sims/{0}_dm_50Mpc/snapshots/'.format(nPoints)
 inDir = dataDir + 'cosmo_sims/{0}_dm_50Mpc/power_spectrum/delta_density/'.format(nPoints)
 outDir = dataDir + 'cosmo_sims/{0}_dm_50Mpc/power_spectrum/fftw_data_double/'.format(nPoints)
+powerDir = dataDir + 'cosmo_sims/{0}_dm_50Mpc/power_spectrum/'.format(nPoints)
+
 create_directory( outDir )
 
 # set simulation volume dimentions
@@ -55,46 +57,76 @@ Lz = Lbox
 dx, dy, dz = Lx/(nx), Ly/(ny), Lz/(nz )
 n_kSamples = 26
 
-
-#Get K_mag
-if index == 0:
-  print 'Computing kx'
-  kx = 2*np.pi*np.fft.fftfreq( nx, d=dx )
-  print 'Computing kz'
-  ky = 2*np.pi*np.fft.fftfreq( ny, d=dy )
-  print 'Computing kz'
-  kz = 2*np.pi*np.fft.fftfreq( nz, d=dz )
-  print 'Shifting K'
-  kx = np.fft.fftshift( kx )
-  ky = np.fft.fftshift( ky )
-  kz = np.fft.fftshift( kz )
-  print 'Computing k Grid'
-  Kz, Ky, Kx = np.meshgrid( kz, ky, kx )
-  print 'Computing K_mag'
-  K_mag = np.sqrt( Kz*Kz + Ky*Ky + Kx*Kx )
-  print 'Saving K_mag'
-  filename = outDir + 'k_mag.h5'
-  file = h5.File( filename, 'w' )
-  file.create_dataset( 'k_mag', data=K_mag )
-  file.create_dataset( 'kx', data=kx )
-  file.create_dataset( 'ky', data=ky )
-  file.create_dataset( 'kz', data=kz )
-  file.close()
-  exit()
  
-if index == 1: snapshots = [ 0, 5, 30 ]
+# if index == 1: snapshots = [ 0, 5, 30 ]
+if index == 1: snapshots = [ 0, ]
 if index == 2: snapshots = [ 60, 90, 120, 150, 169]
 if index == 3: snapshots = [ 150, 169 ]
-# nSnap = 0
+
+
+nSnap = 0
+
+
+
+start = time.time()
+print ' Loading DM Density'
+data_cholla = load_snapshot_data( nSnap, chollaDir, hydro=False, cool=False )
+current_z = data_cholla['current_z']
+dens = data_cholla['dm']['density'][...]
+n_threads = 56
+print ' Computing FFT n_threads:{0}'.format(n_threads)
+power_spectrum, k_vals, count = get_power_spectrum_fftw( dens, Lbox, nx, ny, nz, dx, dy, dz,  n_kSamples=n_kSamples, n_threads=n_threads)
+end = time.time()
+print( ' Elapsed Time: {0:.2f} min'.format((end - start)/60.) )
+
+
+data = np.array([ k_vals, power_spectrum])
+outfile_name = powerDir + 'power_spectrum_{0}.dat'.format(nSnap)
+np.savetxt( outfile_name, data)
+print 'Saved File: ', outfile_name
+
+
+# 
+# #Get K_mag
+# if index == 0:
+#   print 'Computing kx'
+#   kx = 2*np.pi*np.fft.fftfreq( nx, d=dx )
+#   print 'Computing kz'
+#   ky = 2*np.pi*np.fft.fftfreq( ny, d=dy )
+#   print 'Computing kz'
+#   kz = 2*np.pi*np.fft.fftfreq( nz, d=dz )
+#   print 'Shifting K'
+#   kx = np.fft.fftshift( kx )
+#   ky = np.fft.fftshift( ky )
+#   kz = np.fft.fftshift( kz )
+#   print 'Computing k Grid'
+#   Kz, Ky, Kx = np.meshgrid( kz, ky, kx )
+#   print 'Computing K_mag'
+#   K_mag = np.sqrt( Kz*Kz + Ky*Ky + Kx*Kx )
+#   print 'Saving K_mag'
+#   filename = outDir + 'k_mag.h5'
+#   file = h5.File( filename, 'w' )
+#   file.create_dataset( 'k_mag', data=K_mag )
+#   file.create_dataset( 'kx', data=kx )
+#   file.create_dataset( 'ky', data=ky )
+#   file.create_dataset( 'kz', data=kz )
+#   file.close()
+#   exit()
+
+# 
 # for nSnap in snapshots:
 # 
 #   print ' Loading DM Density'
-#   # data_cholla = load_snapshot_data( nSnap, chollaDir, hydro=False, cool=False )
-#   # current_z = data_cholla['current_z']
-#   filename = inDir + 'delta_density_{0}.h5'.format(nSnap)
-#   file = h5.File( filename, 'r' )
-#   delta_dens = file['delta_density'][...]
-#   current_z = file.attrs['current_z'] 
+#   data_cholla = load_snapshot_data( nSnap, chollaDir, hydro=False, cool=False )
+#   current_z = data_cholla['current_z']
+#   dens = data_cholla['dm']['density'][...]
+#   dens_mean = dens_mean
+#   delta_dens = ( dens - dens_mean ) / dens_mean
+#   dens = None
+#   # filename = inDir + 'delta_density_{0}.h5'.format(nSnap)
+#   # file = h5.File( filename, 'r' )
+#   # delta_dens = file['delta_density'][...]
+#   # current_z = file.attrs['current_z'] 
 #   # print delta_dens
 # 
 #   # delta_dens = data_cholla['dm']['density'][...].astype(np.float32)
@@ -102,7 +134,7 @@ if index == 3: snapshots = [ 150, 169 ]
 #   # print dens_mean
 #   # dens = (dens - dens_mean ) / dens_mean
 # 
-#   n_threads = 20
+#   n_threads = 56
 #   print ' Computing FFT n_threads:{0}'.format(n_threads)
 #   start = time.time()
 #   FT = pyfftw.interfaces.numpy_fft.fftn(delta_dens, overwrite_input=True, threads=n_threads)
--- analysis/power_spectrum/power_spectrum.py
+++ analysis/power_spectrum/power_spectrum.py
@@ -81,6 +81,53 @@ def get_delta_k_memory_save( dens, nx, ny, nz, dx, dy, dz ):
   return FT, kx, ky, kz
 
 
+def get_delta_k_fftw( dens, nx, ny, nz, dx, dy, dz, n_threads ):
+  import pyfftw
+  dens_mean = dens.mean()
+  delta_dens = ( dens - dens_mean ) / dens_mean
+  dens = None
+  print '  Computing Fourier Transform'
+  FT = pyfftw.interfaces.numpy_fft.fftn(delta_dens, overwrite_input=True, threads=n_threads)
+  print '   Computing FT Magnitude'
+  FT = FT.real*FT.real + FT.imag*FT.imag
+  print '    Shifting Fourier Transform'
+  FT = np.fft.fftshift(FT)
+  print '    Computing k'
+  fft_kx = 2*np.pi*np.fft.fftfreq( nx, d=dx )
+  fft_ky = 2*np.pi*np.fft.fftfreq( ny, d=dy )
+  fft_kz = 2*np.pi*np.fft.fftfreq( nz, d=dz )
+  print '    Shifting k'
+  kx = np.fft.fftshift( fft_kx )
+  ky = np.fft.fftshift( fft_ky )
+  kz = np.fft.fftshift( fft_kz )
+  return FT, kx, ky, kz
+
+
+def get_power_spectrum_fftw(dens, Lbox, nx, ny, nz, dx, dy, dz, n_kSamples=20, n_threads=1 ):
+#   delta_k2, kx, ky, kz = get_delta_k( dens, nx, ny, nz, dx, dy, dz, n_threads=n_threads )
+  delta_k2, kx, ky, kz = get_delta_k_fftw( dens, nx, ny, nz, dx, dy, dz, n_threads )
+  print '    Computing k grid'
+  Kz, Ky, Kx = np.meshgrid( kz, ky, kx )
+  kx, ky, kz = None, None, None
+  print '    Computing k mag'
+  K_mag = np.sqrt( Kz*Kz + Ky*Ky + Kx*Kx )
+  Kx, Ky, Kz = None, None, None
+  K_mag = K_mag.reshape(K_mag.size)
+  delta_k2 = delta_k2.reshape(delta_k2.size)
+  k_min = (K_mag[np.where(K_mag>0)]).min() * 0.99
+  k_max = K_mag.max()*0.99
+  # print K_mag.max()
+  nBins = n_kSamples
+  intervals = np.logspace(np.log10(k_min), np.log10(k_max), nBins+1)
+  power, bin_edges= np.histogram( K_mag, bins=intervals, weights=delta_k2 )
+  n_in_bin, bin_edges = np.histogram( K_mag, bins=intervals )
+  n_in_bin = n_in_bin.astype('float')
+  bin_centers = np.sqrt(bin_edges[1:] * bin_edges[:-1])
+  power = power / n_in_bin / Lbox**3
+  # error = power * np.sqrt(n_in_bin)
+  return power, bin_centers, n_in_bin
+
+
 def get_power_spectrum(dens, Lbox, nx, ny, nz, dx, dy, dz, n_kSamples=20, n_threads=1 ):
 #   delta_k2, kx, ky, kz = get_delta_k( dens, nx, ny, nz, dx, dy, dz, n_threads=n_threads )
   delta_k2, kx, ky, kz = get_delta_k_memory_save( dens, nx, ny, nz, dx, dy, dz, )
