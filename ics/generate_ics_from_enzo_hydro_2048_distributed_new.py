5c196df3099d1e0aec348fca9b7c1bafe489a776
bruno <bvillasen@gmail.com>
2020-02-22 20:18:28 -0800

    only hydro

--- ics/generate_ics_from_enzo_hydro_2048_distributed_new.py
+++ ics/generate_ics_from_enzo_hydro_2048_distributed_new.py
@@ -55,9 +55,9 @@ domain =  get_domain_block( proc_grid, box_size, grid_size )
 print 'N Cells: {0}'.format(grid_size[0]*grid_size[1]*grid_size[2])
 # fields_particles = ['pos_z', 'vel_x', 'vel_y', 'vel_z'  ]
 # Generate Particles ICs
-fields_particles = ['mass', 'pos_x', 'pos_y', 'pos_z', 'vel_x', 'vel_y', 'vel_z'  ]
-outputBaseName = '{0}_particles.h5'.format(nSnap)
-generate_ics_particles_distributed( fields_particles, domain, proc_grid, data, ds, outputDir, outputBaseName, current_a, current_z, h, get_pid_indices=True, save_pid_indices=True)
+# fields_particles = ['mass', 'pos_x', 'pos_y', 'pos_z', 'vel_x', 'vel_y', 'vel_z'  ]
+# outputBaseName = '{0}_particles.h5'.format(nSnap)
+# generate_ics_particles_distributed( fields_particles, domain, proc_grid, data, ds, outputDir, outputBaseName, current_a, current_z, h, get_pid_indices=True, save_pid_indices=True)
 
 # generate_ics_particles_distributed( fields_particles, domain, proc_grid, data, ds, outputDir, outputBaseName, current_a, current_z, h, get_pid_indices=True)
 # get_pid_indices = False
@@ -71,10 +71,10 @@ generate_ics_particles_distributed( fields_particles, domain, proc_grid, data, d
 # 
 
 # Generate Hydro Ics
-# data_grid = ds.covering_grid( level=0, left_edge=ds.domain_left_edge, dims=ds.domain_dimensions )
-# fields_hydro = [ 'density', 'momentum_x', 'momentum_y', 'momentum_z', 'GasEnergy', 'Energy'] #It has to be in this order
-# outputBaseName = '{0}.h5'.format(nSnap)
-# generate_ics_grid_distributed( fields_hydro, domain, proc_grid, data_grid, ds, outputDir, outputBaseName, current_a, current_z, h )
+data_grid = ds.covering_grid( level=0, left_edge=ds.domain_left_edge, dims=ds.domain_dimensions )
+fields_hydro = [ 'density', 'momentum_x', 'momentum_y', 'momentum_z', 'GasEnergy', 'Energy'] #It has to be in this order
+outputBaseName = '{0}.h5'.format(nSnap)
+generate_ics_grid_distributed( fields_hydro, domain, proc_grid, data_grid, ds, outputDir, outputBaseName, current_a, current_z, h )
 
 
 
